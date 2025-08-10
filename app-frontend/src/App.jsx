7ce5c1bec640646189040b6add3e9c75920a65d9
olasupoolalekanazeez222 <106790709+olasupoolalekanazeez222@users.noreply.github.com>
2025-08-10 05:46:07 -0400

    Update App.jsx

--- app-frontend/src/App.jsx
+++ app-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import Log from "./pages/Log";
-
+import Gmap from "./pages/Gmap";
 export default function App() {
   return (
     <Router>
@@ -10,11 +10,13 @@ export default function App() {
         <Link to="/">Home</Link> |{" "}
         <Link to="/map">Map</Link> |{" "}
         <Link to="/log">Log</Link>
+        <Link to="/gmap">Gmap</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map />} />
         <Route path="/log" element={<Log />} />
+        <Route path="/gmap" element={<Gmap />} />
       </Routes>
     </Router>
   );
